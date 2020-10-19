4302c43625bda494c0e35587b8e4291606ac7d91
Haugaard <mathiashn9@gmail.com>
2020-10-19 11:32:49 +0200

    PersonDTO. Person and Address changes

--- src/main/java/entities/Address.java
+++ src/main/java/entities/Address.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -9,7 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -22,13 +23,13 @@ public class Address implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(length = 45, nullable = false)
     private String street;
-    
+
     @Column(length = 5, nullable = false)
     private int houseNumber;
-    
+
     @Column(length = 10, nullable = true)
     private String floor;
 
@@ -36,18 +37,18 @@ public class Address implements Serializable {
     @ManyToOne(cascade = CascadeType.PERSIST)
     private City city;
 
-    @OneToOne(mappedBy = "address")
-    private Person person;
+    @OneToMany(mappedBy = "address", cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private List<Person> persons;
 
     public Address() {
     }
 
-    public Address(String street, int houseNumber, String floor, City city, Person person) {
+    public Address(String street, int houseNumber, String floor, City city, List<Person> persons) {
         this.street = street;
         this.houseNumber = houseNumber;
         this.floor = floor;
         this.city = city;
-        this.person = person;
+        this.persons = persons;
     }
 
     public Long getId() {
@@ -90,12 +91,20 @@ public class Address implements Serializable {
         this.city = city;
     }
 
-    public Person getPerson() {
-        return person;
+    public List<Person> getPersons() {
+        return persons;
     }
 
-    public void setPerson(Person person) {
-        this.person = person;
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    public void addPerson(Person person) {
+        persons.add(person);
+        
+        if(person != null){
+            person.setAddress(this);
+        }
     }
 
     @Override
@@ -125,12 +134,10 @@ public class Address implements Serializable {
         if (!Objects.equals(this.city, other.city)) {
             return false;
         }
-        if (!Objects.equals(this.person, other.person)) {
+        if (!Objects.equals(this.persons, other.persons)) {
             return false;
         }
         return true;
     }
 
-    
-    
 }
