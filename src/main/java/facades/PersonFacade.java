48635a07b4d47d8a0c193edd62c34508a6263eb7
Nicklas Nielsen <nicklasnielsen2770@gmail.com>
2020-10-19 13:28:15 +0200

    Added required methods for person facade

--- src/main/java/facades/PersonFacade.java
+++ src/main/java/facades/PersonFacade.java
@@ -1,9 +1,12 @@
 package facades;
 
 import dtos.PersonDTO;
+import entities.Person;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 
 /**
  *
@@ -31,12 +34,80 @@ public class PersonFacade {
         return emf.createEntityManager();
     }
 
-    public PersonDTO createPerson() {
-        // TODO
+    private List<PersonDTO> convertToDTO(List<Person> persons) {
+        List<PersonDTO> personDTOs = new ArrayList<>();
+
+        persons.forEach(person -> {
+            personDTOs.add(new PersonDTO(person));
+        });
+
+        return personDTOs;
+    }
+
+    private PersonDTO convertToDTO(Person person) {
+        return new PersonDTO(person);
+    }
+
+    private Person convertFromDTO(PersonDTO personDTO) {
+        // TODO 
+        return null;
+    }
+
+    private boolean incomingDataIsValid(PersonDTO incomingData) {
+        // TODO add validation
+        return false;
+    }
+
+    public PersonDTO createPerson(PersonDTO incomingData) {
+        if (!incomingDataIsValid(incomingData)) {
+            // TODO throw expection
+        }
+
+        EntityManager em = getEntityManager();
+
+        Person person = convertFromDTO(incomingData);
+
+        try {
+            em.getTransaction().begin();
+            em.persist(person);
+            em.getTransaction().commit();
+
+            return convertToDTO(person);
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            // TODO throw expection
+        } finally {
+            em.close();
+        }
+        
+        return null; // To be removed then expection handling have been added
     }
 
     public List<PersonDTO> getPersonsByPhone(int number) {
-        // TODO
+        if (String.valueOf(number).length() != 8) {
+            // TODO throw expection
+        }
+
+        EntityManager em = getEntityManager();
+
+        try {
+            Query query = em.createNamedQuery("Person.getByPhone");
+            query.setParameter("number", number);
+
+            List<Person> persons = query.getResultList();
+            List<PersonDTO> personDTOs = convertToDTO(persons);
+
+            return personDTOs;
+        } catch (Exception e) {
+            // TODO throw expection
+        } finally {
+            em.close();
+        }
+
+        return null; // To be removed then expection handling have been added
     }
-    
+
 }
